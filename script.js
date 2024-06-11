998c4ebaab84ba7c29074a3d8cad470b6ea403eb
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-06-11 12:02:17 +0100

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function payWithMonnify() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const amount = document.getElementById('amount').value;
-    MonnifySDK.initialize({
-      amount: amount,
-      currency: "NGN",
-      reference: "Monn_" + Math.floor((Math.random()* 1000000) + 1),
-      customerFullName: name,
-      customerEmail: email,
-      apiKey: "MK_TEST_P49UUS98XS",
-      contractCode: 5623063227,
-      paymentDescription: "purchase payment",
-      isTestMode: true,
-      onComplete: function(response) {
-        paymentSuccessful = true;
-        if (paymentSuccessful) {
-        window.location.href = "success.html"}
-        else {
-          alert('Transaction not successful')
-        }
-      },
-      onClose: function (data) {
-      return;
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const amount = document.getElementById('amount').value;
+  MonnifySDK.initialize({
+    amount: amount,
+    currency: "NGN",
+    reference: "Monn_" + Math.floor((Math.random()* 1000000) + 1),
+    customerFullName: name,
+    customerEmail: email,
+    apiKey: "MK_TEST_P49UUS98XS",
+    contractCode: 5623063227,
+    paymentDescription: "purchase payment",
+    isTestMode: true,
+    onComplete: function(response) {
+      paymentSuccessful = true;
+      if (paymentSuccessful) {
+      window.location.href = "success.html"}
+      else {
+        alert('Transaction not successful')
       }
-    });
-  };
+    },
+    onClose: function (data) {
+    return;
+    }
+  });
+};
